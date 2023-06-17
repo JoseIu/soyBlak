f12e79cbd7bb23e2eca7576be0227c50ac45c33b
Joselu <joseluissgz97@gmail.com>
2023-06-17 13:42:59 +0200

    adding rel for social links

--- app.js
+++ app.js
@@ -6,15 +6,15 @@ const homeImage = document.querySelector('.home__image');
 const images = ['asstes/images/blak_4.webp', 'asstes/images/blak_3.webp', 'asstes/images/blak.webp'];
 
 const changeImage = index => {
-  console.log(index);
   const imageSRC = images[index];
   homeImage.src = imageSRC;
-  console.log(imageSRC);
 };
-imagesItems.forEach((image, i) => {
-  imagesItems[i].addEventListener('click', () => {
-    imagesItems.forEach(image => image.classList.remove('home__item-focus'));
-    image.classList.add('home__item-focus');
-    changeImage(i);
+document.addEventListener('DOMContentLoaded', () => {
+  imagesItems.forEach((image, i) => {
+    imagesItems[i].addEventListener('click', () => {
+      imagesItems.forEach(image => image.classList.remove('home__item-focus'));
+      image.classList.add('home__item-focus');
+      changeImage(i);
+    });
   });
 });
